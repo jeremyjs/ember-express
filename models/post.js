1825e385d6dfce731f81a28f9b183eeaecb0459a
Jeremy Meyer <jjman505@gmail.com>
2015-06-27 13:52:52 -0400

    added rest adapter

--- models/post.js
+++ models/post.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-    name: String,
-    date: Date,
-    creator: String,
-    content: String
+  name: String,
+  date: Date,
+  creator: String,
+  content: String
 });
 
 module.exports = mongoose.model('Post', PostSchema);
