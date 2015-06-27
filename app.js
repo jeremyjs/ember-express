973f8545d1753c6a42503957f94a5583070e8973
Jeremy Meyer <jjman505@gmail.com>
2015-06-27 13:26:37 -0400

    added mongoose connection

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://root:password@apollo.modulusmongo.net:27017/joB2asiw');
 
 var app = express();
 
